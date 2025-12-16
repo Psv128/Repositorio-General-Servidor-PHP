abd0f9b4c5d6742431e7b6a26fa8825b8b1d7baf
Psv128 <psvsolis@gmail.com>
2025-12-16 17:12:19 +0100

    Add files via upload

---
+++ ejemplo_mvc-main/app/Config.php
@@ -0,0 +1,9 @@
+<?php
+class Config {
+	static public $mvc_bd_hostname = 'localhost';
+	static public $mvc_bd_nombre = 'alimentos';
+	static public $mvc_bd_usuario = 'pablo';
+	static public $mvc_bd_clave = 'pablo123';
+	static public $mvc_vis_css = 'estilo.css';
+}
+?>
